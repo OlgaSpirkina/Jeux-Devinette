dab2c369d25a52d2e7dbb23847b1bd5f39770da1
olga <>
2021-06-07 18:34:42 +0200

    update function with keyCode

--- assets/js/function_bonus.js
+++ assets/js/function_bonus.js
@@ -1,10 +1,6 @@
-// le premier chiffre choisi au hasard
-let displayRandomAnswer = Math.floor(Math.random() * 101);
-console.log("Le chiffre au hasard: " + displayRandomAnswer);
-
 // les différentes variables:
 // est un compteur de clicks du bouton 'Jouer !', serve pour limiter le nombre des tentatives
-let counterOfGuess = 1;
+let counterOfGuess = 0;
 // stock le tableau créé avec chaque nouveau nombre entré. Se remet à 0 en appuyant sur le bouton 'Reset'
 let countEachNumber = document.getElementById('count_times');
 
@@ -14,47 +10,44 @@ function displayRandomNumber(){
   displayRandomAnswer = Math.floor(Math.random() * 101);
   console.log("Le chiffre au hasard: " + displayRandomAnswer);
 }
+window.onload = displayRandomNumber();
 
-// le tableau formé avec le nombre rentrés
-  let answers = [];
-// une variable vide qui va servir plus tard
-  let myNumber = '';
-  function createArray(){
+function createArray(event){
+  var x = event.keyCode;
+  if (x == 13) {
+    event.preventDefault();
+    counterOfGuess++;
     if(counterOfGuess > 5) {
       document.getElementById("demo").innerHTML = "vous avez epuisé vos tentatives, le chiffre est " + displayRandomAnswer;
       return;
     }
+    let answers = [];
+  // une variable vide qui va servir plus tard
+    let myNumber = '';
     myNumber = answers.push(document.getElementById("your_number").value);
     countEachNumber.innerHTML = answers;
-      answers.forEach(compareItem);
-      function compareItem(item){
-        if(isNaN(item) || (item < 0) || (item > 101)){
-          document.getElementById("demo").innerHTML = "Entrez un nombre valide";
-          document.getElementById("paragraph_hidden").style.display = "none";
-          countEachNumber.style.display = "none";
-        }else{
-          countEachNumber.style.display = "block";
-          if(item < displayRandomAnswer){
-            document.getElementById("demo").innerHTML = "trop petit";
-            document.getElementById("paragraph_hidden").style.display = "none";
-          }else if(item > displayRandomAnswer){
-            document.getElementById("demo").innerHTML = "trop grand";
-            document.getElementById("paragraph_hidden").style.display = "none";
-          }
-          else if(item == displayRandomAnswer){
-            document.getElementById("demo").innerHTML = "bravo !";
-            document.getElementById("paragraph_hidden").style.display = "block";
-          }
+    answers.forEach(compareItem);
+    function compareItem(item){
+      if(isNaN(item) || (item < 0) || (item > 101)){
+        document.getElementById("demo").innerHTML = "Entrez un nombre valide";
+        countEachNumber.style.display = "none";
+      }else{
+        countEachNumber.style.display = "block";
+        if(item < displayRandomAnswer){
+          document.getElementById("demo").innerHTML = "trop petit";
+        }else if(item > displayRandomAnswer){
+          document.getElementById("demo").innerHTML = "trop grand";
         }
+        else if(item == displayRandomAnswer){
+          document.getElementById("demo").innerHTML = "bravo ! Vous avez deviné avec : " + counterOfGuess++ + " essais.";
 
+        }
       }
+    }
   }
+}
 
-  document.getElementById("play_btn").addEventListener("focus", counter);
 
-    function counter(){
-    document.getElementById('paragraph_hidden').innerHTML = 'Vous avez deviné avec : ' + counterOfGuess++ + ' essais.';
-  }
 
 
   function resetCounter(){
@@ -64,5 +57,5 @@ function displayRandomNumber(){
     countEachNumber.innerHTML = "";
     answers = [];
     displayRandomNumber();
-    counterOfGuess = 1;
+    counterOfGuess = 0;
   }
